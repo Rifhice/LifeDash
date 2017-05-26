9add3a91a826fefce73aa0e407f0b2936af637af
Kevin giordani <giordani.kevin.02@gmail.com>
2017-05-26 21:05:50 +0200

    Add files via upload

--- v1/LifeDash.sql
+++ v1/LifeDash.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Lun 15 Mai 2017 à 23:00
+-- Généré le :  Ven 26 Mai 2017 à 19:04
 -- Version du serveur :  5.5.54-0+deb8u1
 -- Version de PHP :  5.6.30-0+deb8u1
 
@@ -23,38 +23,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Affiliation`
+-- Structure de la table `AffiliationPerformanceSeance`
 --
 
-CREATE TABLE IF NOT EXISTS `Affiliation` (
+CREATE TABLE IF NOT EXISTS `AffiliationPerformanceSeance` (
+  `IdPerformance` int(11) NOT NULL,
+  `IdSeance` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `AffiliationPerformanceSeance`
+--
+
+INSERT INTO `AffiliationPerformanceSeance` (`IdPerformance`, `IdSeance`) VALUES
+(50, 1),
+(51, 2),
+(52, 2),
+(53, 2),
+(54, 73);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `AffiliationSeanceExercice`
+--
+
+CREATE TABLE IF NOT EXISTS `AffiliationSeanceExercice` (
   `IdSeance` int(11) NOT NULL,
   `IdExercice` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `Affiliation`
+-- Contenu de la table `AffiliationSeanceExercice`
 --
 
-INSERT INTO `Affiliation` (`IdSeance`, `IdExercice`) VALUES
-(1, 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `Charge`
---
-
-CREATE TABLE IF NOT EXISTS `Charge` (
-  `IdExercice` int(11) NOT NULL,
-`Id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
---
--- Contenu de la table `Charge`
---
-
-INSERT INTO `Charge` (`IdExercice`, `Id`) VALUES
-(1, 1);
+INSERT INTO `AffiliationSeanceExercice` (`IdSeance`, `IdExercice`) VALUES
+(2, 2),
+(2, 2),
+(2, 34),
+(2, 2),
+(2, 33),
+(1, 32),
+(73, 55);
 
 -- --------------------------------------------------------
 
@@ -66,16 +76,62 @@ CREATE TABLE IF NOT EXISTS `Exercice` (
 `IdExercice` int(11) NOT NULL,
   `Titre` varchar(128) NOT NULL,
   `Description` varchar(512) NOT NULL,
-  `img` varchar(512) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `Type` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `Exercice`
 --
 
-INSERT INTO `Exercice` (`IdExercice`, `Titre`, `Description`, `img`) VALUES
-(1, 'Développé couché', 'efozrgfierog', 'developer.png'),
-(2, 'Course a pied', 'zehfiopehgherg', 'course.png');
+INSERT INTO `Exercice` (`IdExercice`, `Titre`, `Description`, `Type`) VALUES
+(2, 'Course a pied', 'Great', 2),
+(32, 'Pompes', 'Flexion des brass', 1),
+(33, 'Sprint', 'Court très vite', 2),
+(34, 'Burpees', 'Polyarticulaire', 1),
+(55, 'Dips', 'Travaille les triceps', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `NoteJour`
+--
+
+CREATE TABLE IF NOT EXISTS `NoteJour` (
+  `Jour` date NOT NULL,
+  `Note` int(11) NOT NULL,
+  `Commentaires` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `NoteJour`
+--
+
+INSERT INTO `NoteJour` (`Jour`, `Note`, `Commentaires`) VALUES
+('2017-05-24', 6, 'Ca peut aller'),
+('2017-05-25', 4, 'Meh'),
+('2017-05-26', 8, 'Projet web fini');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Performance`
+--
+
+CREATE TABLE IF NOT EXISTS `Performance` (
+`IdPerformance` int(11) NOT NULL,
+  `IdExercice` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `Performance`
+--
+
+INSERT INTO `Performance` (`IdPerformance`, `IdExercice`) VALUES
+(53, 2),
+(50, 32),
+(52, 33),
+(51, 34),
+(54, 55);
 
 -- --------------------------------------------------------
 
@@ -84,40 +140,43 @@ INSERT INTO `Exercice` (`IdExercice`, `Titre`, `Description`, `img`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `PerformanceCharge` (
-`IdPerf` int(11) NOT NULL,
-  `IdC` int(11) NOT NULL,
-  `Serie` int(11) NOT NULL,
+`IdPerformanceCharge` int(11) NOT NULL,
+  `IdPerformance` int(11) NOT NULL,
+  `Series` int(11) NOT NULL,
   `Repetition` int(11) NOT NULL,
   `Charge` int(11) NOT NULL,
   `Jour` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `PerformanceCharge`
 --
 
-INSERT INTO `PerformanceCharge` (`IdPerf`, `IdC`, `Serie`, `Repetition`, `Charge`, `Jour`) VALUES
-(1, 1, 4, 12, 50, '2017-05-16');
+INSERT INTO `PerformanceCharge` (`IdPerformanceCharge`, `IdPerformance`, `Series`, `Repetition`, `Charge`, `Jour`) VALUES
+(25, 50, 4, 12, 20, '2017-05-26'),
+(26, 51, 4, 12, 0, '2017-05-26'),
+(27, 54, 4, 12, 0, '2017-05-26');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `PerformanceTemp`
+-- Structure de la table `PerformanceTemps`
 --
 
-CREATE TABLE IF NOT EXISTS `PerformanceTemp` (
-`IdPerf` int(11) NOT NULL,
-  `IdT` int(11) NOT NULL,
-  `Temp` time NOT NULL,
+CREATE TABLE IF NOT EXISTS `PerformanceTemps` (
+`IdPerformanceTemps` int(11) NOT NULL,
+  `IdPerformance` int(11) NOT NULL,
+  `Temps` int(11) NOT NULL,
   `Jour` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `PerformanceTemp`
+-- Contenu de la table `PerformanceTemps`
 --
 
-INSERT INTO `PerformanceTemp` (`IdPerf`, `IdT`, `Temp`, `Jour`) VALUES
-(1, 1, '06:00:00', '2017-05-16');
+INSERT INTO `PerformanceTemps` (`IdPerformanceTemps`, `IdPerformance`, `Temps`, `Jour`) VALUES
+(16, 52, 120, '2017-05-26'),
+(17, 53, 900, '2017-05-26');
 
 -- --------------------------------------------------------
 
@@ -129,66 +188,81 @@ CREATE TABLE IF NOT EXISTS `Seance` (
 `IdSeance` int(11) NOT NULL,
   `Titre` varchar(128) NOT NULL,
   `Objectif` varchar(512) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `Seance`
 --
 
 INSERT INTO `Seance` (`IdSeance`, `Titre`, `Objectif`) VALUES
-(1, 'Seance pectoraux', 'Seances avec pour but de faire travailler les pectoraux');
+(1, 'Seance pectoraux', 'On travaille les pectoraux'),
+(2, 'Seance jambes', 'Travailler les jambes'),
+(73, 'Seance triceps', 'Travail des triceps');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Temps`
+-- Structure de la table `TypeExercice`
 --
 
-CREATE TABLE IF NOT EXISTS `Temps` (
-  `IdExercice` int(11) NOT NULL,
-`Id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `TypeExercice` (
+`IdType` int(11) NOT NULL,
+  `type` varchar(64) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `Temps`
+-- Contenu de la table `TypeExercice`
 --
 
-INSERT INTO `Temps` (`IdExercice`, `Id`) VALUES
-(2, 1);
+INSERT INTO `TypeExercice` (`IdType`, `type`) VALUES
+(1, 'charge'),
+(2, 'temps');
 
 --
 -- Index pour les tables exportées
 --
 
 --
--- Index pour la table `Affiliation`
+-- Index pour la table `AffiliationPerformanceSeance`
 --
-ALTER TABLE `Affiliation`
- ADD KEY `IdSeance` (`IdSeance`), ADD KEY `IdExercice` (`IdExercice`);
+ALTER TABLE `AffiliationPerformanceSeance`
+ ADD KEY `IdPerformance` (`IdPerformance`), ADD KEY `IdSeance` (`IdSeance`);
 
 --
--- Index pour la table `Charge`
+-- Index pour la table `AffiliationSeanceExercice`
 --
-ALTER TABLE `Charge`
- ADD PRIMARY KEY (`Id`), ADD KEY `IdExercice` (`IdExercice`);
+ALTER TABLE `AffiliationSeanceExercice`
+ ADD KEY `IdSeance` (`IdSeance`), ADD KEY `IdExercice` (`IdExercice`);
 
 --
 -- Index pour la table `Exercice`
 --
 ALTER TABLE `Exercice`
- ADD PRIMARY KEY (`IdExercice`), ADD KEY `Id` (`IdExercice`), ADD KEY `Id_2` (`IdExercice`), ADD KEY `IdExercice` (`IdExercice`);
+ ADD PRIMARY KEY (`IdExercice`), ADD KEY `Id` (`IdExercice`), ADD KEY `Id_2` (`IdExercice`), ADD KEY `IdExercice` (`IdExercice`), ADD KEY `Type` (`Type`);
+
+--
+-- Index pour la table `NoteJour`
+--
+ALTER TABLE `NoteJour`
+ ADD PRIMARY KEY (`Jour`);
+
+--
+-- Index pour la table `Performance`
+--
+ALTER TABLE `Performance`
+ ADD PRIMARY KEY (`IdPerformance`), ADD KEY `IdExercice` (`IdExercice`);
 
 --
 -- Index pour la table `PerformanceCharge`
 --
 ALTER TABLE `PerformanceCharge`
- ADD PRIMARY KEY (`IdPerf`), ADD KEY `IdC` (`IdC`), ADD KEY `IdC_2` (`IdC`);
+ ADD PRIMARY KEY (`IdPerformanceCharge`), ADD KEY `IdPerformance` (`IdPerformance`);
 
 --
--- Index pour la table `PerformanceTemp`
+-- Index pour la table `PerformanceTemps`
 --
-ALTER TABLE `PerformanceTemp`
- ADD PRIMARY KEY (`IdPerf`), ADD KEY `IdT` (`IdT`);
+ALTER TABLE `PerformanceTemps`
+ ADD PRIMARY KEY (`IdPerformanceTemps`), ADD KEY `IdPerformance` (`IdPerformance`);
 
 --
 -- Index pour la table `Seance`
@@ -197,79 +271,80 @@ ALTER TABLE `Seance`
  ADD PRIMARY KEY (`IdSeance`);
 
 --
--- Index pour la table `Temps`
+-- Index pour la table `TypeExercice`
 --
-ALTER TABLE `Temps`
- ADD PRIMARY KEY (`Id`), ADD UNIQUE KEY `idExo` (`IdExercice`);
+ALTER TABLE `TypeExercice`
+ ADD PRIMARY KEY (`IdType`), ADD KEY `IdType` (`IdType`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT pour la table `Charge`
---
-ALTER TABLE `Charge`
-MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
 -- AUTO_INCREMENT pour la table `Exercice`
 --
 ALTER TABLE `Exercice`
-MODIFY `IdExercice` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `IdExercice` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=56;
+--
+-- AUTO_INCREMENT pour la table `Performance`
+--
+ALTER TABLE `Performance`
+MODIFY `IdPerformance` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=55;
 --
 -- AUTO_INCREMENT pour la table `PerformanceCharge`
 --
 ALTER TABLE `PerformanceCharge`
-MODIFY `IdPerf` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `IdPerformanceCharge` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=28;
 --
--- AUTO_INCREMENT pour la table `PerformanceTemp`
+-- AUTO_INCREMENT pour la table `PerformanceTemps`
 --
-ALTER TABLE `PerformanceTemp`
-MODIFY `IdPerf` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+ALTER TABLE `PerformanceTemps`
+MODIFY `IdPerformanceTemps` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT pour la table `Seance`
 --
 ALTER TABLE `Seance`
-MODIFY `IdSeance` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `IdSeance` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=74;
 --
--- AUTO_INCREMENT pour la table `Temps`
+-- AUTO_INCREMENT pour la table `TypeExercice`
 --
-ALTER TABLE `Temps`
-MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+ALTER TABLE `TypeExercice`
+MODIFY `IdType` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Contraintes pour les tables exportées
 --
 
 --
--- Contraintes pour la table `Affiliation`
+-- Contraintes pour la table `AffiliationPerformanceSeance`
 --
-ALTER TABLE `Affiliation`
-ADD CONSTRAINT `Affiliation_ibfk_2` FOREIGN KEY (`IdExercice`) REFERENCES `Exercice` (`IdExercice`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `Affiliation_ibfk_1` FOREIGN KEY (`IdSeance`) REFERENCES `Seance` (`IdSeance`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `AffiliationPerformanceSeance`
+ADD CONSTRAINT `AffiliationPerformanceSeance_ibfk_1` FOREIGN KEY (`IdPerformance`) REFERENCES `Performance` (`IdPerformance`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `AffiliationPerformanceSeance_ibfk_2` FOREIGN KEY (`IdSeance`) REFERENCES `Seance` (`IdSeance`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `Charge`
+-- Contraintes pour la table `AffiliationSeanceExercice`
 --
-ALTER TABLE `Charge`
-ADD CONSTRAINT `Charge_ibfk_1` FOREIGN KEY (`IdExercice`) REFERENCES `Exercice` (`IdExercice`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `AffiliationSeanceExercice`
+ADD CONSTRAINT `AffiliationSeanceExercice_ibfk_1` FOREIGN KEY (`IdSeance`) REFERENCES `Seance` (`IdSeance`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `AffiliationSeanceExercice_ibfk_2` FOREIGN KEY (`IdExercice`) REFERENCES `Exercice` (`IdExercice`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `Exercice`
+--
+ALTER TABLE `Exercice`
+ADD CONSTRAINT `Exercice_ibfk_1` FOREIGN KEY (`Type`) REFERENCES `TypeExercice` (`IdType`) ON DELETE SET NULL ON UPDATE SET NULL;
 
 --
 -- Contraintes pour la table `PerformanceCharge`
 --
 ALTER TABLE `PerformanceCharge`
-ADD CONSTRAINT `PerformanceCharge_ibfk_1` FOREIGN KEY (`IdC`) REFERENCES `Charge` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `PerformanceCharge_ibfk_1` FOREIGN KEY (`IdPerformance`) REFERENCES `Performance` (`IdPerformance`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `PerformanceTemp`
+-- Contraintes pour la table `PerformanceTemps`
 --
-ALTER TABLE `PerformanceTemp`
-ADD CONSTRAINT `PerformanceTemp_ibfk_1` FOREIGN KEY (`IdT`) REFERENCES `Temps` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `Temps`
---
-ALTER TABLE `Temps`
-ADD CONSTRAINT `Temps_ibfk_1` FOREIGN KEY (`IdExercice`) REFERENCES `Exercice` (`IdExercice`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `PerformanceTemps`
+ADD CONSTRAINT `PerformanceTemps_ibfk_1` FOREIGN KEY (`IdPerformance`) REFERENCES `Performance` (`IdPerformance`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
